4d6b5e9707cc9b76e7f30c04f913c7956895240d
Gbyzzz <Gbyzzz@gmail.com>
2023-02-20 16:41:28 +0200

    some fixes

--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
-mvn clean package && docker compose up -drun  && java -jar target/school-console-app-spring-0.0.1-SNAPSHOT.jar
+mvn clean package && docker compose up -d && java -jar target/school-console-app-spring-0.0.1-SNAPSHOT.jar
+cmd /k
